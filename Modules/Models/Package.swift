04393de8fba3852a01a52690ac28513ed12945d5
Theo Sementa <theosementa@gmail.com>
2026-06-15 11:52:11 +0200

    feat: setup

---
+++ Modules/Models/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Models",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(
+            name: "Models",
+            targets: ["Models"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Models"
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
---
+++ Modules/Models/Sources/Enums/ImageType.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Models
+//
+//  Created by Theo Sementa on 15/06/2026.
+//
+
+import Foundation
+
+// DesignSystem/Sources/Resources/Icons.xcassets
+public enum ImageType {
+    
+    case iconArrowLeft
+    
+}
